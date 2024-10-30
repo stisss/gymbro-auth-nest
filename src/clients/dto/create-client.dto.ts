7833d1112c65754fd9dbbe04e7c78488f65a4c2a
stisss <blazejstiskun@gmail.com>
2024-10-30 21:11:49 +0100

    feat(client): CreateClientDto

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,14 @@
-export class CreateClientDto {}
+import { IsArray, IsOptional, IsString } from 'class-validator';
+
+export class CreateClientDto {
+  @IsString()
+  name: String;
+
+  @IsOptional()
+  @IsArray()
+  redirectUris?: String[];
+
+  @IsOptional()
+  @IsArray()
+  scopes?: String;
+}
