9281044f227b73ec5be351e89208d6f25b0b957c
stisss <blazejstiskun@gmail.com>
2024-11-02 19:38:04 +0100

    feat(auth): jwt basic guard

---
+++ src/auth/guards/jwt-basic-guard.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+import { verifyJwt } from '../utils';
+
+@Injectable()
+export class JwtBasicGuardGuard implements CanActivate {
+  constructor(private readonly configService: ConfigService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const req = context.switchToHttp().getRequest<Request>();
+      const [_, token] = req.headers.authorization.split(' ');
+
+      return !!verifyJwt(token, this.configService.get('JWT_SECRET'));
+    } catch (e) {
+      console.error(e);
+
+      return false;
+    }
+  }
+}
