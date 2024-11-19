7702c6c758c8a42c66fb787ef27d1456ba85a268
stisss <blazejstiskun@gmail.com>
2024-11-19 15:05:14 +0100

    feat(auth): me and logout endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
 import { AuthService } from './auth.service';
+import { JwtBasicGuard } from './guards/jwt-basic.guard';
+import { CustomRequest } from './guards/CustomRequest';
 
 const ACCESS_TOKEN_KEY = 'accessToken';
 const REFRESH_TOKEN_KEY = 'refreshToken';
@@ -36,6 +47,20 @@ export class AuthController {
     res.cookie(ACCESS_TOKEN_KEY, accessToken);
     res.cookie(REFRESH_TOKEN_KEY, refreshToken);
 
+    res.status(HttpStatus.OK);
+  }
+
+  @UseGuards(JwtBasicGuard)
+  @Get('me')
+  me(@Req() req: CustomRequest) {
+    return this.authService.me(req);
+  }
+
+  @UseGuards(JwtBasicGuard)
+  @Get('logout')
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie(ACCESS_TOKEN_KEY);
+    res.clearCookie(REFRESH_TOKEN_KEY);
     res.status(HttpStatus.OK);
   }
 }
